70b4edf8a6d0d6db9f410e048a649844e5500cf3
rakasan <racasan.vlad@gmail.com>
2021-07-14 00:34:23 +0300

    Added signal variance

--- DSP_vs/Source.c
+++ DSP_vs/Source.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 
 #define SIG_LENGTH      320U
 
@@ -7,13 +9,19 @@ extern double InputSignal_f32_1kHz_15kHz[320];
 
 double calc_signal_mean(double* sig_src_arr, int sig_lenght);
 
+double calc_signal_variance(double* sig_src_arr, double sig_mean, int sig_length);
+
 double MEAN;
+
+double VARIANCE;
 
 int main()
 {
     MEAN = calc_signal_mean(&InputSignal_f32_1kHz_15kHz[0], SIG_LENGTH);
 
-    printf("\n\n Mean = %f\n\n\n", MEAN);
+    VARIANCE = calc_signal_variance(&InputSignal_f32_1kHz_15kHz[0], MEAN, SIG_LENGTH);
+
+    printf("\n\n Variance = %f\n\n\n", VARIANCE);
 
     return 0;
 }
@@ -29,4 +37,19 @@ double calc_signal_mean(double* sig_src_arr, int sig_lenght)
     _mean = (_mean / (double)sig_lenght);
 
     return _mean;
+}
+
+double calc_signal_variance(double* sig_src_arr, double sig_mean, int sig_length)
+{
+    double _variance = 0.0;
+
+    for (int i = 0; i < sig_length; i++)
+    {
+       // _variance = _variance + ((sig_src_arr[i] - sig_mean) * (sig_src_arr[i] - sig_mean));
+        _variance = _variance + pow((sig_src_arr[i] - sig_mean), 2);
+    }
+
+    _variance = _variance / (double)((double)sig_length - 1);
+
+    return _variance;
 }
